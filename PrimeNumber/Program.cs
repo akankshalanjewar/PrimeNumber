66e540372bd084001496a289c5d408ac4e13fbe1
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-12 12:50:38 +0530

    [Akanksha]addedprimeNumber

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Num;
+            Console.Write("Enter  the number: ");
+            Num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= Num / 2; i++)
+            {
+                if (Num % 2==0)
+                {
+                    Console.WriteLine(" is not a  prime number"); 
+                }
+                else
+                {
+                    Console.WriteLine("is a prime number");
+                   
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
