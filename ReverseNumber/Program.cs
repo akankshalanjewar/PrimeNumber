7c84fa996b80678fb5f475a242746c7009c69615
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-12 13:16:04 +0530

    added[akanksha]reverse file

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.Write("Enter the Number:  ");
+            Num = int.Parse(Console.ReadLine());
+
+            int reverse = 0;
+
+           while (Num > 0)
+            {
+                int Output = (Num % 10);
+                reverse = reverse * 10 + Output;
+                Num = (Num / 10);
+
+
+            }
+            Console.WriteLine(+  reverse  + "  is a reverse of a number  ");
+            Console.ReadLine();
+        }
+    }
+}
